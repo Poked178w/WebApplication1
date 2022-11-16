bb1375e36dd21990cf0d98213e1957b82ff518c5
ABC <airbitdisaster74k@gmail.com>
2022-11-16 23:52:56 +0300

    Исправление всех ошибок

---
+++ WebApplication1/Migrations/20221116204428_StartMigrationV1.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PartyInvites.Migrations
+{
+    public partial class StartMigrationV1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NameClan = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clans", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Platforms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PlatformN = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Platforms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Nickname = table.Column<string>(type: "text", nullable: false),
+                    Platform = table.Column<string>(type: "text", nullable: false),
+                    ClanId = table.Column<string>(type: "text", nullable: true),
+                    Clan = table.Column<string>(type: "text", nullable: true),
+                    Score = table.Column<int>(type: "integer", nullable: false),
+                    Victories = table.Column<int>(type: "integer", nullable: false),
+                    ClanId1 = table.Column<int>(type: "integer", nullable: true),
+                    PlatformId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Players_Clans_ClanId1",
+                        column: x => x.ClanId1,
+                        principalTable: "Clans",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Players_Platforms_PlatformId",
+                        column: x => x.PlatformId,
+                        principalTable: "Platforms",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_ClanId1",
+                table: "Players",
+                column: "ClanId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_PlatformId",
+                table: "Players",
+                column: "PlatformId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Clans");
+
+            migrationBuilder.DropTable(
+                name: "Platforms");
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,11 +1,12 @@
+using Microsoft.AspNet.Identity;
 using Microsoft.EntityFrameworkCore;
 using PartyInvites.Data;
 using PartyInvites.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-const string connection = "Server = (localdb)\\mssqllocaldb;Database = userstoredb;Trusted_Connection=true";
-builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+const string connection = "User ID=postgres; Password = Poked178w; Host = localhost; Port = 5432; Database = myDataBase";
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
 builder.Services.AddScoped<PlayersService>();
 
 builder.Services.AddControllersWithViews();
